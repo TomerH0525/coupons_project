760f400c252b6705f842fc0a64309d4d29008f1c
TomerH <tomer2b0525@gmail.com>
2025-06-19 16:05:03 +0300

    added some pages, and deleted unnecessary css that came with vite

---
+++ src/components/HomePage/Home.tsx
@@ -0,0 +1,13 @@
+
+
+
+function Home(){
+
+    return(
+        <>
+        <h1>Blah Blah</h1>
+        </>
+    )
+}
+
+export default Home
---
+++ src/components/Layout/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+
+
+function Footer(){
+
+    return(
+        <>
+        <div style={{margin:"auto",}}>
+            <h4>This is a footer!</h4>
+        </div>
+        </>
+
+    )
+}
+
+export default Footer
